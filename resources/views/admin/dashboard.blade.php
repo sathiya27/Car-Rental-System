03cf9eafb97c8d280f5a2f9ae31f8dbe0cbeb8b4
SathiyaS <selan.sathiya16@gmail.com>
2024-10-17 12:02:56 +0800

    Login and registration using breeze

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+</head>
+<body>
+    <h1>Admin Dashboard</h1>
+    <form method="POST" action="{{ route('logout') }}">
+        @csrf
+
+        <x-dropdown-link :href="route('logout')"
+                onclick="event.preventDefault();
+                            this.closest('form').submit();">
+            {{ __('Log Out') }}
+        </x-dropdown-link>
+    </form>
+</body>
+</html>
