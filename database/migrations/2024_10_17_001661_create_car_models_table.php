2ccb43c4b063d786824ca1124d6de9a347edf898
SathiyaS <selan.sathiya16@gmail.com>
2024-10-17 10:01:09 +0800

    Added migration implementation and models

---
+++ database/migrations/2024_10_17_001661_create_car_models_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('car_models', function (Blueprint $table) {
+            $table->id();
+            $table->string('modelName');
+            $table->integer('numOfSeats')->unsigned();
+            $table->integer('numOfDoors');
+            $table->string('fuelType');
+            $table->boolean('hasAc')->default(true);
+            $table->string('carImage');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('car_models');
+    }
+};
---
+++ database/migrations/2024_10_17_001723_create_bookings_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->foreignId('car_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('pick_up_location_id')->constrained('locations')->cascadeOnDelete();
+            $table->foreignId('drop_off_location_id')->constrained('locations')->cascadeOnDelete();
+            $table->dateTime('pick_up_dateTime');
+            $table->dateTime('drop_off_dateTime');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
